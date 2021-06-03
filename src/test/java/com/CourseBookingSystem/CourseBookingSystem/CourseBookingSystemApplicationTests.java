3df2e56f94e427d5e96b4fc5221a4f31082d8511
konrad <ktalaga90@gmail.com>
2021-06-03 11:18:31 +0100

    project started

---
+++ src/test/java/com/CourseBookingSystem/CourseBookingSystem/CourseBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CourseBookingSystem.CourseBookingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
